90657fa6357e13ab550c6a8fcc7e6ee3d140effd
Hee-jin506 <gmlwls35220@gmail.com>
2021-07-12 19:30:21 +0900

    express 위에 socket.io, peer.js 올리기

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+const { v4: uuidV4 } = require('uuid');
+
+/* GET home page. */
+router.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`);
+});
+
+router.get('/:room', (req, res) => {
+  res.render('index', { roomId: req.params.room });
+});
+
+module.exports = router;
